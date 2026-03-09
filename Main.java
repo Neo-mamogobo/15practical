32e6d097749a9bc91b6e85b79373ac612d42ef4e
Cscience1 <dipinamamogobo@gmail.com>
2026-03-09 15:16:02 +0200

    Start anagram project

--- Main.java
+++ Main.java
@@ -1,6 +1,45 @@
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static String Signature(String word){
+        char[] arr = word.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
     public static void main(String[] args) {
+        HashMap<String, Integer> wordcount = new HashMap<>();
+        HashMap<String, ArrayList<String>> anagram = new HashMap<>();
 
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("ulysses.txt"));
+            String line;
+
+            while((line = reader.readLine()) != null){
+                line = line.toLowerCase();
+                line = line.replaceAll("[^a-zA-z']", " ");
+                String[] words = line.split("\\s+");
+
+                for(String word : words){
+                    if(word.isEmpty()) continue;
+                    if(wordcount.containsKey(word)){
+                        wordcount.put(word, wordcount.get(word) + 1);
+                    }else {
+                        wordcount.put(word, 1);
+                    }
+                    String sig = signature(word);
+                    if(!anagram.containsKey(sig)){
+                        anagram.put(sig, new ArrayList<>());
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static String signature(String word) {
+        char[] arr = word.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
     }
 }
